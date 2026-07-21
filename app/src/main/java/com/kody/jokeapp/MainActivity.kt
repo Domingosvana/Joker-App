a5ddf0eba921641bde330bb5f27968ccde0c6352
SeuNomeDeUsuarioNoGitHub <seuemail@exemplo.com>
2026-07-21 23:36:41 +0100

    implementado funcao de navegacao

--- app/src/main/java/com/kody/jokeapp/MainActivity.kt
+++ app/src/main/java/com/kody/jokeapp/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // enableEdgeToEdge()
@@ -16,7 +23,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout = findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.main)
+        val navView = findViewById<com.google.android.material.navigation.NavigationView>(R.id.nav_view)
+
+       val navController = findNavController(R.id.nav_host_fragment_container_main)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_joke_day, R.id.nav_about
+            ), drawerLayout
+        )
+
+    setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 
 
 
