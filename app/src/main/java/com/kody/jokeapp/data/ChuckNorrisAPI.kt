d90c9762d7c4200dcb7ba2fb96ecbe64506b1b72
SeuNomeDeUsuarioNoGitHub <seuemail@exemplo.com>
2026-08-19 17:13:07 +0100

    adicionar interface ChuckNorrisAPI com busca de categorias

--- app/src/main/java/com/kody/jokeapp/data/ChuckNorrisAPI.kt
+++ app/src/main/java/com/kody/jokeapp/data/ChuckNorrisAPI.kt
@@ -1,4 +1,13 @@
 package com.kody.jokeapp.data
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ChuckNorrisAPI {
+
+    @GET("jokes/categories")
+    fun findAllCategories(@Query("apiKey") apiKey: String/* = HTTPClient.API_KEY*/  ): Call<List<String>>
+
+
 }
